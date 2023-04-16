546d42257ba906d8dec5b97b87618a025315d516
NuocDua <dungga7541000@gmail.com>
2023-04-17 03:21:51 +0700

    design website mvp model

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const userRoute = require("./routes/users.js");
 const movieRoute = require("./routes/movies.js");
 const listRoute = require("./routes/lists.js");
 
+
 dotenv.config();
 const host ="127.0.0.1";
 const PORT = process.env.PORT || 8000;
@@ -20,7 +21,9 @@ mongoose.connect(process.env.MONGODB_URL ,{
 }).then(()=>console.log("DB connection successfully"));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+
+
+app.use(cors)
 
 
 app.use("/api/auth",authRoute)
